6f8a42a41884aaf1a12509884a18cd065efd03c0
Harold Velasquez <hvelasquez@unicauca.edu.co>
2025-04-27 15:11:18 -0500

    otra vez

--- src/main/java/tech/xirius/payment/infrastructure/persistence/entity/WalletEntity.java
+++ src/main/java/tech/xirius/payment/infrastructure/persistence/entity/WalletEntity.java
@@ -1,28 +1,32 @@
 package tech.xirius.payment.infrastructure.persistence.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+/**
+ * Entidad que representa la billetera de un usuario.
+ */
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
-@Table(name = "wallet")
+@Table(name = "wallet", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "user_id")
+})
 public class WalletEntity {
 
     @Id
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 
-    @Column(name = "user_id", nullable = false, unique = true)
+    @Column(name = "user_id", nullable = false, unique = true, length = 255)
     private String userId;
 
     @Column(nullable = false, precision = 38, scale = 2)
     private BigDecimal balance;
-
 }
